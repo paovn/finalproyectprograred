ebfb1ccb241d5b665b64d5562cb738ec0acd1033
Sebastian Becerra <sbecerraz10@gmail.com>
2019-11-22 15:36:12 -0500

    JAVA SOURCE UPDATE 3

--- FundacionValleDelLili/NPFinalProject/src/main/java/config/ApplicationConfig.java
+++ FundacionValleDelLili/NPFinalProject/src/main/java/config/ApplicationConfig.java
@@ -1,5 +1,26 @@
 package config;
 
-public class ApplicationConfig {
+import java.util.HashSet;
+import java.util.Set;
 
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+import services.DoctorServices;
+import services.GeneralServices;
+import services.PatientServices;
+
+
+@ApplicationPath("web")
+public class ApplicationConfig extends Application{
+	
+	@Override
+	public Set<Class<?>> getClasses(){
+		Set<Class<?>> recursos = new HashSet<Class<?>>();
+		recursos.add(DoctorServices.class);
+		recursos.add(PatientServices.class);
+		recursos.add(GeneralServices.class);
+		return recursos;
+		
+	}
 }
